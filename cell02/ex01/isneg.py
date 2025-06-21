1473f2715c488a6d756c332bed37e3d3126e870e
lalita06197 <lalita06197@gmail.com>
2025-06-21 16:19:07 +0000

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+x = input("type a number: ")
+y = "0"
+if x > y:
+    print("This number is positive")
+if x < y:
+    print("This number is negative")
+if x == y:
+    print("This number is both positive and negative")
