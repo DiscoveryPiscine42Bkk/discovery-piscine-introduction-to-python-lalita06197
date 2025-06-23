2824dd5b21a962f2a84f6be6ba54e3d913d7352e
lalita06197 <lalita06197@gmail.com>
2025-06-23 03:13:36 +0000

    play_with_arrays.py

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import array
+ori_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+for number in ori_array:
+    new_array.append(number + 2)
+print("Original array: ", ori_array)
+print("New array: ", new_array)
